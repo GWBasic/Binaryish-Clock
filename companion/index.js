602b8c827cdd1a9ed86b5a048931fb194dbc7614
Andrew Rondeau <GWBasic@users.noreply.github.com>
2018-10-04 21:53:21 -0400

    Added default settings

--- companion/index.js
+++ companion/index.js
@@ -3,9 +3,34 @@ import * as messaging from "messaging";
 
 const enumSettings = ["upperLeft", "upperRight", "lowerLeft", "lowerRight"];
 
+// Uncomment to clear settings
+/*
+settingsStorage.removeItem("bitText");
+settingsStorage.removeItem("upperLeft");
+settingsStorage.removeItem("upperRight");
+settingsStorage.removeItem("lowerLeft");
+settingsStorage.removeItem("lowerRight");*/
+
+setDefaultSetting("bitText", true);
+setDefaultSetting("upperLeft", {"values":[{"name":"Date","value":"date"}],"selected":[0]});
+setDefaultSetting("upperRight", {"values":[{"name":"(Empty)","value":null}],"selected":[6]});
+setDefaultSetting("lowerLeft", {"values":[{"name":"(Empty)","value":null}],"selected":[6]});
+setDefaultSetting("lowerRight", {"values":[{"name":"Time","value":"time"}],"selected":[1]});
+
+function setDefaultSetting(key, value) {
+  let extantValue = settingsStorage.getItem(key);
+  if (extantValue == null) {
+    console.log(`Set default value ${key}: ${JSON.stringify(value)}`);
+    settingsStorage.setItem(key, JSON.stringify(value));
+  } else {
+    console.log(`Loaded ${key}: ${JSON.stringify(extantValue)}`);
+  }
+}
+
 settingsStorage.onchange = function(evt) {
   
   console.log(`Updated setting: ${JSON.stringify(evt)}`);
+  //console.log(`New Setting: ${evt.key}: ${evt.newValue}`);
   
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
     
